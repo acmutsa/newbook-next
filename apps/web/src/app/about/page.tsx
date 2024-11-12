5b92d7851d747179ffefd27d871451edfbba1835
Matthew <86093074+matthewmuriel@users.noreply.github.com>
2024-11-12 07:27:25 -0600

    Add about directory
    added about directory and created page.tsx with placeholder text

---
+++ apps/web/src/app/about/page.tsx
@@ -0,0 +1,33 @@
+export default function Page() {
+	return (
+		<div className="mx-auto w-screen max-w-7xl px-5 pt-20 text-utsa-blue">
+			<div className="items-left flex flex-col py-8">
+				<h1 className="font-eb text-6xl font-black text-utsa-blue">
+					About Us
+				</h1>
+			</div>
+			<div className="grid grid-cols-2 grid-rows-3 gap-6">
+				<p className="col-start-1 row-start-1">
+					Lorem ipsum odor amet, consectetuer adipiscing elit. Mi a
+					mus arcu ultricies quisque tincidunt. Sagittis quis
+					consequat eget justo habitasse. Fermentum massa egestas
+					consequat placerat; nulla himenaeos pretium. Pharetra
+					pulvinar augue dolor imperdiet sed suspendisse finibus velit
+					duis. Dictum aptent lacinia convallis commodo libero etiam.
+					Ante interdum rhoncus eu gravida consequat auctor. Sociosqu
+					natoque diam erat justo lacus cubilia aliquet.
+				</p>
+				<p className="col-start-1 row-start-2">
+					Pulvinar aptent euismod tortor penatibus velit tellus
+					sollicitudin ac nibh. Primis sagittis cubilia suspendisse
+					interdum senectus ultricies quisque urna semper. Dui ad
+					finibus auctor varius maximus arcu curae. Taciti ex platea
+					sodales venenatis purus blandit? Luctus sollicitudin ligula
+					placerat gravida fames arcu quis. Orci cursus in at
+					pellentesque etiam ligula at? Dictum laoreet rutrum a etiam
+					pharetra laoreet sociosqu netus.
+				</p>
+			</div>
+		</div>
+	);
+}
