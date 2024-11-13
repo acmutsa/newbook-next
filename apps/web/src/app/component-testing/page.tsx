5afb21a839ac922048533db9c4e65b7b8206b5d1
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2024-11-12 18:31:52 -0600

    Renamed AdvisorTitleCard to ProfileTitleCard to make it more generic; added some fallbacks in case anything's null

--- apps/web/src/app/component-testing/page.tsx
+++ apps/web/src/app/component-testing/page.tsx
@@ -1,16 +1,35 @@
-import AdvisorScore from "@/components/shared/advisor/NewBookAdvisorScore";
+import AdvisorScoreCard from "@/components/shared/advisor/NewBookAdvisorScore";
+import ProfileTitleCard from "@/components/shared/NewBookProfileTitle";
 
 
 export default function Page() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center bg-offwhite">
-            <h1 className="pb-8">AdvisorScore</h1>
-            <div className="flex gap-8">
-                <AdvisorScore score={0.2} />
-                <AdvisorScore score={1.314} />
-                <AdvisorScore score={2.314} />
-                <AdvisorScore score={3.58} />
-                <AdvisorScore score={4.19} />
+            <h1>AdvisorScoreCard</h1>
+            <div className="flex gap-8 my-8">
+                <AdvisorScoreCard score={0.2} />
+                <AdvisorScoreCard score={1.314} />
+                <AdvisorScoreCard score={2.314} />
+                <AdvisorScoreCard score={3.58} />
+                <AdvisorScoreCard score={4.19} />
+            </div>
+            <h1>ProfileTitleCard</h1>
+            <div className="flex flex-col gap-8 my-8">
+                <ProfileTitleCard
+                    name="Professor Name"
+                    title="Assistant Professor of Instruction"
+                    unit="Department of Computer Science"
+                />
+                <ProfileTitleCard
+                    name="Lecturer Name"
+                    title="Lecturer"
+                    unit="Department of Computer Science"
+                />
+                <ProfileTitleCard
+                    name="Advisor Name"
+                    title="Academic advisor"
+                    unit="College of Sciences"
+                />
             </div>
         </main>
     ); 
