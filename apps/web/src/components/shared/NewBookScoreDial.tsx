2f65a5f4a22d5562945f799ba65c3b7f354a9e82
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2024-11-15 18:53:53 -0600

    Began work on score dial component; tweaked styling for score icon for user reviews

--- apps/web/src/components/shared/NewBookScoreDial.tsx
+++ apps/web/src/components/shared/NewBookScoreDial.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // React imports
 import { PropsWithChildren } from "react";
 // shadcn components
@@ -79,3 +80,73 @@ export default function ScoreDialCard({
 		</Card>
 	);
 }
+=======
+import clsx from "clsx";
+import {
+    Card,
+    CardTitle,
+    CardContent
+} from "@/components/ui/card";
+import { IconProps } from "@radix-ui/react-icons/dist/types";
+import { RocketIcon } from "@radix-ui/react-icons";
+import { createElement, PropsWithChildren } from "react";
+
+const MAX_SCORE = 1;
+
+export default function ScoreDial({
+    score,
+    children,
+}: PropsWithChildren<{
+    score: number;
+}>) {
+    const scorePercent = score / MAX_SCORE;
+    const size = 100; // TODO: make this a prop
+    const outerRadius = 5; // TODO: make this a prop? somehow
+    const innerRadius = ( outerRadius * 2 ) - 2; // TODO: make this a prop
+    const strokeFill = ( Math.PI * outerRadius );
+    const strokeFillPercent = strokeFill * scorePercent;
+
+    return (
+        <Card className="flex flex-col items-center p-4">
+            <svg className={clsx("rotate-180",
+                    {
+                        "stroke-score-0": ( scorePercent >= 0 ) && ( scorePercent < 0.2 ),
+                        "stroke-score-1": ( scorePercent >= 0.2 ) && ( scorePercent < 0.4 ),
+                        "stroke-score-2": ( scorePercent >= 0.4 ) && ( scorePercent < 0.6 ),
+                        "stroke-score-3": ( scorePercent >= 0.6 ) && ( scorePercent < 0.8 ),
+                        "stroke-score-4": ( scorePercent >= 0.8 ),
+                    },
+                )}
+                height={size}
+                width={size}
+                viewBox="0 0 20 20"
+            >
+                <circle
+                    r={outerRadius}
+                    cx="10"
+                    cy="10"
+                    stroke-width="10"
+                    stroke="#ededed"
+                    stroke-dasharray={`${strokeFill} ${strokeFill * 2}`}
+                />
+                <circle
+                    r={outerRadius}
+                    cx="10"
+                    cy="10"
+                    stroke-width="10"
+                    stroke-dasharray={`${strokeFillPercent} ${strokeFill * 2}`}
+                />
+                <circle r={innerRadius} cx="10" cy="10" fill="white" stroke-width="0" />
+            </svg>
+            <div className="flex flex-col items-center -mt-[4.25rem] z-10">
+                <div className="font-bold text-xl">
+                    {(scorePercent * 100).toFixed()}%
+                </div>
+                <div className="flex gap-1 items-center">
+                    {children}
+                </div>
+            </div>
+        </Card>
+    )
+}
+>>>>>>> ecf4bf3 (Began work on score dial component; tweaked styling for score icon for user reviews)
--- apps/web/src/components/shared/NewBookScoreIcon.tsx
+++ apps/web/src/components/shared/NewBookScoreIcon.tsx
@@ -22,6 +22,7 @@ export default function ScoreIcon({ score }: { score: number }) {
 					? PROFILE_MAX_OVERALL_SCORE
 					: score / PROFILE_MAX_OVERALL_SCORE;
 
+<<<<<<< HEAD
 	return (
 		<Avatar>
 			<AvatarFallback
@@ -45,3 +46,31 @@ export default function ScoreIcon({ score }: { score: number }) {
 		</Avatar>
 	);
 }
+=======
+export default function ScoreIcon({
+    score
+}: {
+    score: number;
+}) {
+    // TODO: error-checking for invalid score
+    // TODO: Implement logic for actually pulling score from the database w/ the key, instead of just taking it in as a prop
+    const scorePercent = score / MAX_SCORE;
+
+    return (
+        <Avatar>
+            <AvatarFallback className={clsx(
+                "font-bold shadow-[inset_0_0_0.25em_0.05em_#000]",
+                {
+                    "shadow-score-0": ( scorePercent >= 0 ) && ( scorePercent < 0.2 ),
+                    "shadow-score-1": ( scorePercent >= 0.2 ) && ( scorePercent < 0.4 ),
+                    "shadow-score-2": ( scorePercent >= 0.4 ) && ( scorePercent < 0.6 ),
+                    "shadow-score-3": ( scorePercent >= 0.6 ) && ( scorePercent < 0.8 ),
+                    "shadow-score-4": ( scorePercent >= 0.8 ),
+                },
+            )}>
+                { score.toFixed(1) ?? NaN }
+            </AvatarFallback>
+        </Avatar>
+    );
+}
+>>>>>>> ecf4bf3 (Began work on score dial component; tweaked styling for score icon for user reviews)
