04ed4effef658013c919c5055e72897377cc9d07
Matthew <86093074+matthewmuriel@users.noreply.github.com>
2024-11-19 18:49:48 -0600

    add text to about us.

--- apps/web/src/app/about/page.tsx
+++ apps/web/src/app/about/page.tsx
@@ -12,26 +12,29 @@ export default function Page() {
 			<div className="items-left flex flex-col py-8">
 				<h1 className="font-eb text-6xl font-black">About Us</h1>
 			</div>
-			<div className="grid grid-cols-3 grid-rows-2 gap-10">
+			<div className="grid grid-cols-3 grid-rows-2 gap-6">
 				<p className="col-span-3 col-start-1 row-start-1 md:col-span-1">
-					Lorem ipsum odor amet, consectetuer adipiscing elit. Mi a
-					mus arcu ultricies quisque tincidunt. Sagittis quis
-					consequat eget justo habitasse. Fermentum massa egestas
-					consequat placerat; nulla himenaeos pretium. Pharetra
-					pulvinar augue dolor imperdiet sed suspendisse finibus velit
-					duis. Dictum aptent lacinia convallis commodo libero etiam.
-					Ante interdum rhoncus eu gravida consequat auctor. Sociosqu
-					natoque diam erat justo lacus cubilia aliquet.
+					NewBook was a platform designed by UTSA students, for UTSA
+					students!<br></br>
+					<br></br> Our project goal was to make the process of
+					exploring and evaluating UTSA courses, faculty, and advisors
+					simple, insightful, and student focused. Whether you’re
+					planning your schedule or seeking advice, NewBook helps you
+					make informed decisions with real reviews and ratings from
+					your peers.
 				</p>
 				<p className="col-span-3 row-start-2 md:col-span-1">
-					Pulvinar aptent euismod tortor penatibus velit tellus
-					sollicitudin ac nibh. Primis sagittis cubilia suspendisse
-					interdum senectus ultricies quisque urna semper. Dui ad
-					finibus auctor varius maximus arcu curae. Taciti ex platea
-					sodales venenatis purus blandit? Luctus sollicitudin ligula
-					placerat gravida fames arcu quis. Orci cursus in at
-					pellentesque etiam ligula at? Dictum laoreet rutrum a etiam
-					pharetra laoreet sociosqu netus.
+					This project was created in Fall 2024 by members of UTSA’s
+					Association for Computing Machinery (ACM). As a
+					collaborative effort of passionate students, we aimed to
+					address a common challenge faced by our fellow
+					Roadrunners—navigating academic options with limited or
+					outdated information.<br></br>
+					<br></br>
+					<b>
+						Together, we’re building a stronger, more informed UTSA
+						community.
+					</b>
 				</p>
 				<Image
 					src="/sp1.jpg"
@@ -49,9 +52,11 @@ export default function Page() {
 			<div className="">
 				<Accordion type="single" collapsible>
 					<AccordionItem value="item-1">
-						<AccordionTrigger>Is it accessible?</AccordionTrigger>
+						<AccordionTrigger>
+							Are you affiliated with UTSA?
+						</AccordionTrigger>
 						<AccordionContent>
-							Yes. It adheres to the WAI-ARIA design pattern.
+							No. This project has no affiliation with UTSA.
 						</AccordionContent>
 					</AccordionItem>
 				</Accordion>
