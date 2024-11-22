1834aef8bf36d97cbff8e98ced3831f0005fd9ca
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2024-11-22 16:25:24 -0600

    Added error-checking for no given score on score dial; added to-do items

--- apps/web/src/components/shared/NewBookScoreDial.tsx
+++ apps/web/src/components/shared/NewBookScoreDial.tsx
@@ -91,24 +91,25 @@ import { PropsWithChildren } from "react";
 
 const MAX_SCORE = 1;
 
-// TODO: the rest of the white circle should not be visible on a smaller container. instead, make it a half-circle; use a gradient fill for this.
 export default function ScoreDial({
     score,
     children,
 }: PropsWithChildren<{
     score: number;
 }>) {
-    const scorePercent = ( ( score >= 0 ) && ( score <= MAX_SCORE )) ? score / MAX_SCORE : MAX_SCORE;
+    // Check if score is given, and if percent is in range, and hit corresponding min/max otherwise
+    const scorePercent = ( score == undefined ) ? NaN
+                                                : ( score < 0 ) ? 0
+                                                                : ( score > MAX_SCORE ) ? MAX_SCORE
+                                                                                        : score / MAX_SCORE;
     const size = 100; // TODO: make this a prop
     const outerRadius = 5; // TODO: make this a prop? somehow
-    const innerRadius = ( outerRadius * 2 ) - 2; // TODO: make this a prop
     const strokeFill = ( Math.PI * outerRadius );
     const strokeFillPercent = strokeFill * scorePercent;
 
     return (
         <Card className="flex flex-col items-center p-4">
             <svg className={clsx("rotate-180",
-            // <svg className={clsx("",
                     {
                         "stroke-score-0": ( scorePercent >= 0 ) && ( scorePercent < 0.2 ),
                         "stroke-score-1": ( scorePercent >= 0.2 ) && ( scorePercent < 0.4 ),
@@ -132,7 +133,7 @@ export default function ScoreDial({
                     cx="10"
                     cy="10"
                     stroke-width="10"
-                    stroke-dasharray={`${strokeFillPercent} ${strokeFill * 2}`}
+                    stroke-dasharray={`${strokeFillPercent ?? 0} ${strokeFill * 2}`}
                 />
                 <path
                     fill="#ffffff"
@@ -143,7 +144,7 @@ export default function ScoreDial({
             </svg>
             <div className="flex flex-col items-center -mt-[4.25rem] z-10">
                 <div className="font-bold text-xl -mt-1">
-                    {(scorePercent * 100).toFixed()}%
+                    {scorePercent ? ((scorePercent * 100).toFixed() + "%") : "N/A"}
                 </div>
                 <div className="flex gap-1 items-center">
                     {children}
