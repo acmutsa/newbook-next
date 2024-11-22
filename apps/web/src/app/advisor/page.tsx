d1ff5be95f5b569985ac6c8bf4a84dfbb2e8c73a
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2024-11-22 00:55:08 -0600

    Finished work on title card; added to-do item on review card; changed layout on advisor page

--- apps/web/src/app/advisor/page.tsx
+++ apps/web/src/app/advisor/page.tsx
@@ -1,3 +1,4 @@
+import ProfileReview from "@/components/shared/NewBookProfileReview"
 import ProfileTitleCard from "@/components/shared/NewBookProfileTitle"
 import ScoreCard from "@/components/shared/NewBookScoreCard"
 import ScoreDial from "@/components/shared/NewBookScoreDial"
@@ -6,10 +7,10 @@ import { CalendarCheck, SquareCheckBig, Laugh, HeartHandshake } from "lucide-rea
 
 export default function Page() {
     return( 
-        <main className="flex flex-col min-h-screen items-center justify-center">
-            <div className="grid grid-cols-2 gap-2">
+        <main className="container mx-auto flex flex-col min-h-screen justify-center">
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-2">
                 {/* Desktop left-side pane */}
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-2 lg:col-span-5">
                     <div className="flex gap-2">
                         <ProfileTitleCard
                             name="Advisor Name"
@@ -23,31 +24,44 @@ export default function Page() {
                             <CardTitle>Category ratings</CardTitle>
                         </CardHeader>
                         <CardContent>
-                            <div className="grid grid-cols-2 gap-2">
+                            <div className="grid grid-cols-2 xl:grid-cols-4 gap-2">
                                 <ScoreDial score={0.82}>
-                                    <CalendarCheck size="1em" /> Responsiveness
+                                    <CalendarCheck size="1em" /> Responsive
                                 </ScoreDial>
                                 <ScoreDial score={0.64}>
-                                    <SquareCheckBig size="1em" /> Accuracy
+                                    <SquareCheckBig size="1em" /> Accurate
                                 </ScoreDial>
                                 <ScoreDial score={1.0}>
-                                    <Laugh size="1em" /> Approachability
+                                    <Laugh size="1em" /> Approachable
                                 </ScoreDial>
                                 <ScoreDial score={0.9}>
-                                    <HeartHandshake size="1em" /> Helpfulness
+                                    <HeartHandshake size="1em" /> Helpful
                                 </ScoreDial>
                             </div>
                         </CardContent>
                     </Card>
                 </div>
                 {/* Desktop right-side pane */}
-                <div>
+                <div className="lg:col-span-7">
                     <Card>
                         <CardHeader>
                             <CardTitle>Reviews</CardTitle>
                         </CardHeader>
-                        <CardContent>
-                            
+                        <CardContent className="flex flex-col gap-2">
+                            <ProfileReview
+                                score={5.0}
+                                major="Biology"
+                                classOf={2024}
+                                datePosted={new Date(1732084730000)}
+                                content="This advisor was awesome!"
+                            />
+                            <ProfileReview
+                                score={3.5}
+                                major="Computer science"
+                                classOf={2024}
+                                datePosted={new Date(1732024030000)}
+                                content="They were nice, but they forgot to tell me this deadline that I almost missed. Double-check your DegreeWorks!"
+                            />
                         </CardContent>
                     </Card>
                 </div>
