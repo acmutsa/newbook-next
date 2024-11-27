4ac530237d3785bfd94d59dd929673452eaf2894
Rodrigo Davila Castillo <26052518+gataria@users.noreply.github.com>
2024-11-27 12:53:54 -0600

    Ran prettier formatting command

--- apps/web/src/app/advisor/page.tsx
+++ apps/web/src/app/advisor/page.tsx
@@ -2,20 +2,26 @@
 
 // Test data
 import {
-    advisorOne,
-    advisorOneCategoryRatings,
-    ratingOne,
-    ratingTwo,
+	advisorOne,
+	advisorOneCategoryRatings,
+	ratingOne,
+	ratingTwo,
 } from "@/lib/test-data";
 // Newbook components
-import ProfileReview from "@/components/shared/NewBookProfileReview"
-import ProfileTitleCard from "@/components/shared/NewBookProfileTitle"
-import ScoreCard from "@/components/shared/NewBookScoreCard"
-import ScoreDialCard from "@/components/shared/NewBookScoreDial"
+import ProfileReview from "@/components/shared/NewBookProfileReview";
+import ProfileTitleCard from "@/components/shared/NewBookProfileTitle";
+import ScoreCard from "@/components/shared/NewBookScoreCard";
+import ScoreDialCard from "@/components/shared/NewBookScoreDial";
 // shadcn components
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 // Icon components
-import { CalendarCheck, SquareCheckBig, Laugh, HeartHandshake, Plus } from "lucide-react"
+import {
+	CalendarCheck,
+	SquareCheckBig,
+	Laugh,
+	HeartHandshake,
+	Plus,
+} from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 /**
@@ -23,77 +29,90 @@ import { Button } from "@/components/ui/button";
  * @returns A React page component.
  */
 export default function Page() {
-    const advisorReviews = [ratingOne, ratingTwo];
-    const advisorProfileData = advisorOne;
-    const advisorCategoryData = advisorOneCategoryRatings;
-    
-    return ( 
-        <main className="container mx-auto flex flex-col min-h-screen justify-center">
-            <div className="grid grid-cols-1 lg:grid-cols-12 gap-2">
-                {/* Desktop left-side pane */}
-                <div className="flex flex-col gap-2 md:col-span-6 xl:col-span-5">
-                    <div className="flex gap-2">
-                        <ProfileTitleCard
-                            name={advisorProfileData.firstname + " " + advisorProfileData.lastname}
-                            title={advisorProfileData.title}
-                            unit={advisorProfileData.unit}
-                        />
-                        <ScoreCard score={advisorProfileData.score} />
-                    </div>
-                    <Card>
-                        <CardHeader>
-                            <CardTitle>Category ratings</CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            <div className="grid grid-cols-2 xl:grid-cols-4 gap-2">
-                                <ScoreDialCard score={advisorCategoryData.responsive}>
-                                    <CalendarCheck size="1em" /> Responsive
-                                </ScoreDialCard>
-                                <ScoreDialCard score={advisorCategoryData.accurate}>
-                                    <SquareCheckBig size="1em" /> Accurate
-                                </ScoreDialCard>
-                                <ScoreDialCard score={advisorCategoryData.approachable}>
-                                    <Laugh size="1em" /> Approachable
-                                </ScoreDialCard>
-                                <ScoreDialCard score={advisorCategoryData.helpful}>
-                                    <HeartHandshake size="1em" /> Helpful
-                                </ScoreDialCard>
-                            </div>
-                        </CardContent>
-                    </Card>
-                </div>
-                {/* Desktop right-side pane */}
-                <div className="md:col-span-6 xl:col-span-7">
-                    <Card>
-                        <CardHeader>
-                            <CardTitle className="flex items-center">
-                                <div className="flex-1 justify-self-start">
-                                    Reviews
-                                </div>
-                                <div className="justify-self-end">
-                                    <Button>
-                                        Add review <Plus className="ml-1" size="1.25em" />
-                                    </Button>
-                                </div>
-                            </CardTitle>
-                        </CardHeader>
-                        <CardContent className="flex flex-col gap-2">
-                            {( advisorReviews.length > 0 )  ? advisorReviews.map((review) =>
-                                                                    <ProfileReview
-                                                                        score={review.score}
-                                                                        major={review.student.major}
-                                                                        classOf={review.student.classOf}
-                                                                        datePosted={review.datePosted}
-                                                                    >
-                                                                        {review.content}
-                                                                    </ProfileReview>
-                                                                )
-                                                            : "No reviews have been made for this advisor."
-                            }
-                        </CardContent>
-                    </Card>
-                </div>
-            </div>
-        </main>
-    )
+	const advisorReviews = [ratingOne, ratingTwo];
+	const advisorProfileData = advisorOne;
+	const advisorCategoryData = advisorOneCategoryRatings;
+
+	return (
+		<main className="container mx-auto flex min-h-screen flex-col justify-center">
+			<div className="grid grid-cols-1 gap-2 lg:grid-cols-12">
+				{/* Desktop left-side pane */}
+				<div className="flex flex-col gap-2 md:col-span-6 xl:col-span-5">
+					<div className="flex gap-2">
+						<ProfileTitleCard
+							name={
+								advisorProfileData.firstname +
+								" " +
+								advisorProfileData.lastname
+							}
+							title={advisorProfileData.title}
+							unit={advisorProfileData.unit}
+						/>
+						<ScoreCard score={advisorProfileData.score} />
+					</div>
+					<Card>
+						<CardHeader>
+							<CardTitle>Category ratings</CardTitle>
+						</CardHeader>
+						<CardContent>
+							<div className="grid grid-cols-2 gap-2 xl:grid-cols-4">
+								<ScoreDialCard
+									score={advisorCategoryData.responsive}
+								>
+									<CalendarCheck size="1em" /> Responsive
+								</ScoreDialCard>
+								<ScoreDialCard
+									score={advisorCategoryData.accurate}
+								>
+									<SquareCheckBig size="1em" /> Accurate
+								</ScoreDialCard>
+								<ScoreDialCard
+									score={advisorCategoryData.approachable}
+								>
+									<Laugh size="1em" /> Approachable
+								</ScoreDialCard>
+								<ScoreDialCard
+									score={advisorCategoryData.helpful}
+								>
+									<HeartHandshake size="1em" /> Helpful
+								</ScoreDialCard>
+							</div>
+						</CardContent>
+					</Card>
+				</div>
+				{/* Desktop right-side pane */}
+				<div className="md:col-span-6 xl:col-span-7">
+					<Card>
+						<CardHeader>
+							<CardTitle className="flex items-center">
+								<div className="flex-1 justify-self-start">
+									Reviews
+								</div>
+								<div className="justify-self-end">
+									<Button>
+										Add review{" "}
+										<Plus className="ml-1" size="1.25em" />
+									</Button>
+								</div>
+							</CardTitle>
+						</CardHeader>
+						<CardContent className="flex flex-col gap-2">
+							{advisorReviews.length > 0
+								? advisorReviews.map((review) => (
+										<ProfileReview
+											score={review.score}
+											major={review.student.major}
+											classOf={review.student.classOf}
+											datePosted={review.datePosted}
+										>
+											{review.content}
+										</ProfileReview>
+									))
+								: "No reviews have been made for this advisor."}
+						</CardContent>
+					</Card>
+				</div>
+			</div>
+		</main>
+	);
 }
--- apps/web/src/components/shared/NewBookProfileReview.tsx
+++ apps/web/src/components/shared/NewBookProfileReview.tsx
@@ -4,12 +4,12 @@
 import { PropsWithChildren, useState } from "react";
 // shadcn components
 import {
-    Card,
-    CardHeader,
-    CardContent,
-    CardFooter,
-    CardTitle,
-    CardDescription
+	Card,
+	CardHeader,
+	CardContent,
+	CardFooter,
+	CardTitle,
+	CardDescription,
 } from "@/components/ui/card";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 // Newbook components
@@ -33,73 +33,82 @@ type HelpfulValues = "helpful" | "not-helpful" | "";
  * @returns A React profile review card component.
  */
 export default function ProfileReview({
-    score,
-    major,
-    classOf,
-    datePosted,
-    helpfulInitState,
-    children,
-    onHelpfulToggle
+	score,
+	major,
+	classOf,
+	datePosted,
+	helpfulInitState,
+	children,
+	onHelpfulToggle,
 }: PropsWithChildren<{
-    score: number;
-    major: string;
-    classOf: number;
-    datePosted: Date;
-    helpfulInitState?: HelpfulValues;
-    onHelpfulToggle?: (value: HelpfulValues) => void;
+	score: number;
+	major: string;
+	classOf: number;
+	datePosted: Date;
+	helpfulInitState?: HelpfulValues;
+	onHelpfulToggle?: (value: HelpfulValues) => void;
 }>) {
-    const [helpfulState, setHelpfulState] = useState<HelpfulValues>(helpfulInitState ?? "");
+	const [helpfulState, setHelpfulState] = useState<HelpfulValues>(
+		helpfulInitState ?? "",
+	);
 
-    function changeValue(value: string) {
-        switch (value) {
-            case "helpful":
-            case "not-helpful":
-                setHelpfulState(value);
-                onHelpfulToggle?.(value);
-                break;
-            default:
-                setHelpfulState("");
-                onHelpfulToggle?.("");
-        }
-    }
+	function changeValue(value: string) {
+		switch (value) {
+			case "helpful":
+			case "not-helpful":
+				setHelpfulState(value);
+				onHelpfulToggle?.(value);
+				break;
+			default:
+				setHelpfulState("");
+				onHelpfulToggle?.("");
+		}
+	}
 
-    return (
-        <Card>
-            <CardHeader>
-                <div className="flex justify-between">
-                    <div className="flex space-x-3 items-center">
-                        <ScoreIcon score={score} />
-                        <div>
-                            <CardTitle>Student</CardTitle>
-                            <CardDescription>{ major ?? "major" } | Class of { classOf ?? "classOf" }</CardDescription>
-                        </div>
-                    </div>
-                    <div>
-                        {/* Add date in local "medium" formatting -- en_US => Nov 14, 2024 */}
-                        { datePosted?.toLocaleDateString(undefined, { dateStyle: 'medium' }) ?? "datePosted" }
-                    </div>
-                </div>
-            </CardHeader>
-            <CardContent>
-                { children }
-            </CardContent>
-            <CardFooter className="text-sm space-x-2">
-                <div>
-                    Was this review helpful?
-                </div>
-                <ToggleGroup
-                    type="single"
-                    value={helpfulState}
-                    onValueChange={(value) => changeValue(value)}
-                >
-                    <ToggleGroupItem value="helpful" aria-label="Toggle helpful">
-                        <ThumbsUp />
-                    </ToggleGroupItem>
-                    <ToggleGroupItem value="not-helpful" aria-label="Toggle not helpful">
-                        <ThumbsDown />
-                    </ToggleGroupItem>
-                </ToggleGroup>
-            </CardFooter>
-        </Card>
-    )
+	return (
+		<Card>
+			<CardHeader>
+				<div className="flex justify-between">
+					<div className="flex items-center space-x-3">
+						<ScoreIcon score={score} />
+						<div>
+							<CardTitle>Student</CardTitle>
+							<CardDescription>
+								{major ?? "major"} | Class of{" "}
+								{classOf ?? "classOf"}
+							</CardDescription>
+						</div>
+					</div>
+					<div>
+						{/* Add date in local "medium" formatting -- en_US => Nov 14, 2024 */}
+						{datePosted?.toLocaleDateString(undefined, {
+							dateStyle: "medium",
+						}) ?? "datePosted"}
+					</div>
+				</div>
+			</CardHeader>
+			<CardContent>{children}</CardContent>
+			<CardFooter className="space-x-2 text-sm">
+				<div>Was this review helpful?</div>
+				<ToggleGroup
+					type="single"
+					value={helpfulState}
+					onValueChange={(value) => changeValue(value)}
+				>
+					<ToggleGroupItem
+						value="helpful"
+						aria-label="Toggle helpful"
+					>
+						<ThumbsUp />
+					</ToggleGroupItem>
+					<ToggleGroupItem
+						value="not-helpful"
+						aria-label="Toggle not helpful"
+					>
+						<ThumbsDown />
+					</ToggleGroupItem>
+				</ToggleGroup>
+			</CardFooter>
+		</Card>
+	);
 }
