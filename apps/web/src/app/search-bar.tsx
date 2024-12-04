e79ecc721aa1b4d43d336d3b44d3f8313f289d2e
meisullum <meisullum@gmail.com>
2024-12-04 10:58:49 -0600

    loading state

--- apps/web/src/app/search-bar.tsx
+++ apps/web/src/app/search-bar.tsx
@@ -1,34 +1,60 @@
 "use client";
 import { Search } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function SearchBar() {
 	const [searchValue, setSearchValue] = useState("");
+	const [isLoading, setLoading] = useState(false);
+
 	const router = useRouter();
 
-	function triggerSearch() {
+	useEffect (() => {
+		// console.log("Am I loading? " + isLoading);
+	}, [isLoading] );
+
+	// function wait(ms: number) {
+	// 	return new Promise(resolve=>setTimeout(resolve,ms));
+	// }
+
+	async function triggerSearch() {
 		const sv = searchValue.trim();
 		if (sv.length > 0) {
-			router.push(`/search?q=${encodeURIComponent(sv)}`);
+			setLoading(true);
+			await router.push(`/search?q=${encodeURIComponent(sv)}`);
+			// await wait(5000);
+			setLoading(false);
 		}
 	}
 
 	return (
-		<div className="flex h-16 gap-x-2">
-			<input
-				onChange={(e) => setSearchValue(e.target.value)}
-				onKeyDown={(e) => {
-					if (e.key === "Enter") triggerSearch();
-				}}
-				className="h-full w-screen max-w-[500px] rounded-xl border border-utsa-blue bg-utsa-blue/15 px-5 font-noto text-xl font-semibold !text-utsa-blue text-black"
-			></input>
-			<button
-				onClick={triggerSearch}
-				className="flex aspect-square h-full items-center justify-center rounded-xl border border-utsa-blue bg-utsa-blue/15"
-			>
-				<Search color="#0C2440" />
-			</button>
+
+		<div>
+
+			{isLoading && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+                    <div className="loader"></div>
+                </div>
+            )}
+
+			<div className="flex h-16 gap-x-2">
+				<input
+					onChange={(e) => setSearchValue(e.target.value)}
+					onKeyDown={(e) => {
+						if (e.key === "Enter") triggerSearch();
+					}}
+					disabled={isLoading}
+					className="h-full w-screen max-w-[500px] rounded-xl border border-utsa-blue bg-utsa-blue/15 px-5 font-noto text-xl font-semibold !text-utsa-blue text-black"
+				></input>
+				<button
+					onClick={triggerSearch}
+					className="flex aspect-square h-full items-center justify-center rounded-xl border border-utsa-blue bg-utsa-blue/15"
+				>
+					<Search color="#0C2440" />
+				</button>
+			</div>
 		</div>
+		
+		
 	);
 }
