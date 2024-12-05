b74327fe2e503644af8679f9bc100b133f1e2e31
Liam Murray <liam@murray.to>
2024-12-05 17:35:49 -0600

    formatting

--- packages/db/drizzle/schema.ts
+++ packages/db/drizzle/schema.ts
@@ -1,8 +1,13 @@
-import { pgTable, serial, text, integer, boolean, timestamp, unique } from "drizzle-orm/pg-core"
-  import { sql } from "drizzle-orm"
-
-
-
+import {
+	pgTable,
+	serial,
+	text,
+	integer,
+	boolean,
+	timestamp,
+	unique,
+} from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
 
 export const courseInstructorRatings = pgTable("course_instructor_ratings", {
 	id: serial("id").primaryKey().notNull(),
@@ -13,21 +18,28 @@ export const courseInstructorRatings = pgTable("course_instructor_ratings", {
 	content: text("content"),
 	grade: text("grade"),
 	wasMandatoryAttend: boolean("was_mandatory_attend"),
-	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow().notNull(),
+	createdAt: timestamp("created_at", { mode: "string" })
+		.defaultNow()
+		.notNull(),
 });
 
-export const instructors = pgTable("instructors", {
-	id: serial("id").primaryKey().notNull(),
-	title: text("title").notNull(),
-	firstname: text("firstname").notNull(),
-	lastname: text("lastname").notNull(),
-	scrapedString: text("scraped_string").notNull(),
-},
-(table) => {
-	return {
-		instructorsScrapedStringUnique: unique("instructors_scraped_string_unique").on(table.scrapedString),
-	}
-});
+export const instructors = pgTable(
+	"instructors",
+	{
+		id: serial("id").primaryKey().notNull(),
+		title: text("title").notNull(),
+		firstname: text("firstname").notNull(),
+		lastname: text("lastname").notNull(),
+		scrapedString: text("scraped_string").notNull(),
+	},
+	(table) => {
+		return {
+			instructorsScrapedStringUnique: unique(
+				"instructors_scraped_string_unique",
+			).on(table.scrapedString),
+		};
+	},
+);
 
 export const advisorRatings = pgTable("advisor_ratings", {
 	id: serial("id").primaryKey().notNull(),
@@ -35,22 +47,29 @@ export const advisorRatings = pgTable("advisor_ratings", {
 	advisorId: integer("advisor_id").notNull(),
 	ratingValue: integer("rating_value").notNull(),
 	content: text("content"),
-	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow().notNull(),
+	createdAt: timestamp("created_at", { mode: "string" })
+		.defaultNow()
+		.notNull(),
 });
 
-export const users = pgTable("users", {
-	clerkId: text("clerk_id"),
-	firstname: text("firstname").notNull(),
-	lastname: text("lastname").notNull(),
-	username: text("username").notNull(),
-	email: text("email").notNull(),
-	profileImage: text("profile_image"),
-},
-(table) => {
-	return {
-		usersUsernameUnique: unique("users_username_unique").on(table.username),
-	}
-});
+export const users = pgTable(
+	"users",
+	{
+		clerkId: text("clerk_id"),
+		firstname: text("firstname").notNull(),
+		lastname: text("lastname").notNull(),
+		username: text("username").notNull(),
+		email: text("email").notNull(),
+		profileImage: text("profile_image"),
+	},
+	(table) => {
+		return {
+			usersUsernameUnique: unique("users_username_unique").on(
+				table.username,
+			),
+		};
+	},
+);
 
 export const courses = pgTable("courses", {
 	id: serial("id").primaryKey().notNull(),
