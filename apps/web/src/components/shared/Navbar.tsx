b373a8f676a5ee5ac6b7dea51b47a2c981c028f9
Liam Murray <liam@murray.to>
2024-12-06 01:24:48 -0600

    Add Alpha Tag

--- apps/web/src/components/shared/Navbar.tsx
+++ apps/web/src/components/shared/Navbar.tsx
@@ -8,12 +8,15 @@ export default function Navbar() {
 	return (
 		<nav className="fixed top-0 h-20 w-screen">
 			<div className="mx-auto grid h-full w-full max-w-7xl grid-cols-2 px-5">
-				<div className="flex items-center">
+				<div className="flex items-center gap-x-2">
 					<Link href={"/"}>
 						<h1 className="font-eb text-xl font-bold text-utsa-blue">
 							NewBook
 						</h1>
 					</Link>
+					<span className="rounded-sm border-2 border-utsa-blue px-1 text-[0.5rem] font-semibold">
+						ALPHA
+					</span>
 				</div>
 				<div className="flex items-center justify-end gap-x-4 font-eb text-lg font-semibold text-utsa-blue">
 					<Link href={"https://acmutsa.org/"}>ACM</Link>
