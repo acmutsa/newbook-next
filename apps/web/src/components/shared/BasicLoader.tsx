f29f3f7dfd0a5679655ea7e35c629389c7435cff
Christian Walker <christiandwalker2021@gmail.com>
2024-12-10 02:03:24 -0600

    creates basic loader

---
+++ apps/web/src/components/shared/BasicLoader.tsx
@@ -0,0 +1,9 @@
+export default function BasicLoader(){
+  return (
+		<div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-20">
+			<div className="flex animate-bounce items-center justify-center text-5xl">
+				🐸
+			</div>
+		</div>
+  );
+}
