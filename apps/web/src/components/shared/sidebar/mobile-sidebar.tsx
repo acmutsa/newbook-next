54d3a76d3b8b2de0c75b7d9574ae0688a3c6b1f9
Matthew <86093074+matthewmuriel@users.noreply.github.com>
2024-12-12 15:57:59 -0600

    remove redundant files

--- apps/web/src/components/shared/sidebar/mobile-sidebar.tsx
+++ apps/web/src/components/shared/sidebar/mobile-sidebar.tsx
@@ -1,9 +1,14 @@
 import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+import { SidebarHeaderContent } from "./sidebar-header-content";
+import { SidebarFooterContent } from "./sidebar-footer-content";
+import { SidebarMainContent } from "./sidebar-content";
 
 export function MobileSidebar() {
 	return (
 		<Sidebar side="right">
-			<SidebarContent />
+			<SidebarHeaderContent />
+			<SidebarMainContent />
+			<SidebarFooterContent />
 		</Sidebar>
 	);
 }
