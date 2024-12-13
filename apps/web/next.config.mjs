830e1f6803f8086ddceed02b1fbdf4fffee0d003
Christian Walker <christiandwalker2021@gmail.com>
2024-12-13 13:30:07 -0600

    adds clerk hostname fix

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+		domains: [
+			"images.clerk.dev",
+			"www.gravatar.com",
+			"img.clerk.com",
+			"api.dicebear.com",
+			"cdn.discordapp.com",
+		],
+  }
+};
 
 export default nextConfig;
