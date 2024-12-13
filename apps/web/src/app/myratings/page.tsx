b3237204dbb4a9a01b63e9137df45b1634e20509
joshuasilva414 <joshuasilva414@gmail.com>
2024-12-12 18:08:27 -0600

    switch to score dial

--- apps/web/src/app/myratings/page.tsx
+++ apps/web/src/app/myratings/page.tsx
@@ -1,25 +1,78 @@
-import RatingCard from "@/components/shared/RatingCard";
+import { AdvisorRating, CourseRating } from "@/components/shared/RatingCard";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { currentUser, redirectToSignIn } from "@clerk/nextjs/server";
+import { getMyRatings } from "db/queries";
 
 export default async function Page() {
-	// const ratings = await getMyRatings(userID);
+	const user = await currentUser();
+
+	if (!user || !user.id) {
+		return redirectToSignIn();
+	}
+
+	// const { advisors, courses } = await getMyRatings(user.id);
+
+	// This query isn't currently working, so lets make some advisor and course rating arrays
+	const advisors = [
+		{
+			id: "1",
+			content: "This advisor was great!",
+			createdAt: new Date(),
+			ratingValue: 5,
+			advisor: {
+				name: "Dr. Advisor",
+			},
+		},
+		{
+			id: "2",
+			content: "This advisor was also great!",
+			createdAt: new Date(),
+			ratingValue: 5,
+			advisor: {
+				name: "Dr. Advisor",
+			},
+		},
+	];
+
+	const courses = [
+		{
+			id: "1",
+			content: "This course was great!",
+			createdAt: new Date(),
+			ratingValue: 4,
+			title: "Course Title",
+		},
+		{
+			id: "2",
+			content: "This course was also great!",
+			createdAt: new Date(),
+			ratingValue: 2,
+			title: "Course Title",
+		},
+	];
 
 	return (
-		<div className="mx-8 mt-20 pt-8">
+		<div className="mx-8 mt-20 w-full pt-8">
 			<h1 className="mb-16 text-center font-serif text-5xl font-bold">
 				My Ratings
 			</h1>
-			<div>
+			<div className="text-left">
 				<h2 className="mb-4 px-8 text-4xl font-bold">
-					Professor Ratings
+					Advisor Ratings
 				</h2>
 				<div className="grid grid-cols-1 gap-4 overflow-x-scroll p-8 md:grid-cols-2 lg:grid-cols-3">
-					<RatingCard id="1" />
-					<RatingCard id="2" />
-					<RatingCard id="3" />
-					<RatingCard id="4" />
-					<RatingCard id="5" />
+					{advisors.map((a) => (
+						<AdvisorRating advisor={a} />
+					))}
+				</div>
+			</div>
+			<div className="text-left">
+				<h2 className="mb-4 px-8 text-4xl font-bold">Course Ratings</h2>
+				<div className="grid grid-cols-1 gap-4 overflow-x-scroll p-8 md:grid-cols-2 lg:grid-cols-3">
+					{courses.map((c) => (
+						<CourseRating course={c} />
+					))}
 				</div>
 			</div>
 		</div>
--- apps/web/src/components/shared/RatingCard.tsx
+++ apps/web/src/components/shared/RatingCard.tsx
@@ -8,41 +8,102 @@ import {
 	CardTitle,
 } from "../ui/card";
 import { Button } from "../ui/button";
+import NewBookScoreDial from "./NewBookScoreDial";
 
-type RatingCardProps = {
+interface CourseRatingProps {
 	id: string;
-};
-
-export default function RatingCard({ id }: RatingCardProps) {
+	content: string;
+	createdAt: Date;
+	ratingValue: number;
+	title: string;
+}
+export function CourseRating({
+	course: { id, title, content, createdAt, ratingValue },
+}: {
+	course: CourseRatingProps;
+}) {
 	return (
 		<Card className="min-w-[32ch] max-w-[64ch] transition-shadow duration-200 hover:shadow-xl">
 			<CardHeader>
 				<div className="flex items-center justify-between">
 					<Link href={`/myratings/${id}`}>
-						<CardTitle>Course Title</CardTitle>
+						<CardTitle>{title}</CardTitle>
+					</Link>
+					<span
+						className="inline-flex items-center text-amber-400"
+						aria-hidden="true"
+					>
+						<NewBookScoreDial
+							score={ratingValue}
+						></NewBookScoreDial>
+					</span>
+				</div>
+			</CardHeader>
+			<CardContent>
+				<p>{content}</p>
+			</CardContent>
+			<CardFooter>
+				<div className="flex w-full items-center justify-end">
+					{/* <Link href={`/myratings/${id}/edit`}>
+						<Button variant="outline">Edit</Button>
+					</Link> */}
+					<p className="text-sm italic text-gray-500">
+						{createdAt.toDateString()}
+					</p>
+				</div>
+			</CardFooter>
+		</Card>
+	);
+}
+
+interface AdvisorRatingProps {
+	id: string;
+	content: string;
+	createdAt: Date;
+	ratingValue: number;
+	advisor: {
+		name: string;
+	};
+}
+export function AdvisorRating({
+	advisor: {
+		id,
+		content,
+		createdAt,
+		ratingValue,
+		advisor: { name },
+	},
+}: {
+	advisor: AdvisorRatingProps;
+}) {
+	return (
+		<Card className="min-w-[32ch] max-w-[64ch] transition-shadow duration-200 hover:shadow-xl">
+			<CardHeader>
+				<div className="flex items-center justify-between">
+					<Link href={`/myratings/${id}`}>
+						<CardTitle>{name}</CardTitle>
 					</Link>
 					<span
 						className="inline-flex items-center text-amber-500"
 						aria-hidden="true"
 					>
-						{Array.from({ length: 5 }).map((_, index) => (
-							<Star key={index} size={16} />
-						))}
+						<NewBookScoreDial
+							score={ratingValue}
+						></NewBookScoreDial>
 					</span>
 				</div>
 			</CardHeader>
 			<CardContent>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Quisquam, quos.
-				</p>
+				<p>{content}</p>
 			</CardContent>
 			<CardFooter>
-				<div className="flex w-full items-center justify-between">
-					<Link href={`/myratings/${id}/edit`}>
+				<div className="flex w-full items-center justify-end">
+					{/* <Link href={`/myratings/${id}/edit`}>
 						<Button variant="outline">Edit</Button>
-					</Link>
-					<p className="text-sm italic text-gray-500">11/11/11</p>
+					</Link> */}
+					<p className="text-sm italic text-gray-500">
+						{createdAt.toDateString()}
+					</p>
 				</div>
 			</CardFooter>
 		</Card>
